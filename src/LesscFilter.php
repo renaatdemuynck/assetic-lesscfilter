baa402dfb4df3c25efb5db963544c40499f1677a
Renaat De Muynck <renaat.demuynck@gmail.com>
2017-06-14 12:03:31 +0200

    Add first version of the filter

---
+++ src/LesscFilter.php
@@ -0,0 +1,38 @@
+<?php
+namespace RDM\Assetic\Filter;
+
+use Assetic\Filter\BaseProcessFilter;
+use Assetic\Asset\AssetInterface;
+use Assetic\Exception\FilterException;
+
+/**
+ * Lessc filter
+ */
+class LesscFilter extends BaseProcessFilter
+{
+
+    private $lesscBin;
+
+    public function __construct($lesscBin = '/usr/bin/lessc')
+    {
+        $this->lesscBin = $lesscBin;
+    }
+
+    public function filterLoad(AssetInterface $asset)
+    {}
+
+    public function filterDump(AssetInterface $asset)
+    {
+        $pb = $this->createProcessBuilder([$this->lesscBin, '-']);
+        $proc = $pb->getProcess();
+        $proc->setInput($asset->getContent());
+        
+        $code = $proc->run();
+        
+        if ($code !== 0) {
+            throw FilterException::fromProcess($proc)->setInput($asset->getContent());
+        }
+        
+        $asset->setContent($proc->getOutput());
+    }
+}
